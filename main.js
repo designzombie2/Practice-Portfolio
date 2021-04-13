4602a64a425b8aba91e76a0c2fe249db69d6cffb
designzombie2 <82090836+designzombie2@users.noreply.github.com>
2021-04-13 15:23:57 +0900

    JS:Nabar

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-const toggleBtn = document.querySelector('.navbar__toggleBtn');
-const menu = document.querySelector('.navbar__menu');
+'use strict';
 
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    console.log(`navbarHeight: ${navbarHeight}`);
+    if(window.scrollY > navbarHeight){
+        navbar.classList.add('navbar--dark');
+    } else{
+        navbar.classList.remove('navbar--dark');
+    }
 });
