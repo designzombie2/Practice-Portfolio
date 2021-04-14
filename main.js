58bae46aa83eec328753e678a1845c7da76f896d
designzombie2 <82090836+designzombie2@users.noreply.github.com>
2021-04-14 09:36:04 +0900

    JS:project filtering

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    // console.log(window.scrollY);
-    // console.log(`navbarHeight: ${navbarHeight}`);
+    console.log(window.scrollY);
+    console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
     } else{
@@ -17,14 +17,14 @@ document.addEventListener('scroll', () => {
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-    // console.log(event.target.dataset.link);
+    console.log(event.target.dataset.link);
     const target = event.target;
     const link = target.dataset.link;
     if (link == null) {
         return;
     }
 
-    // console.log(event.target.dataset.link);
+    console.log(event.target.dataset.link);
     scrollIntoView(link);
 });
 
@@ -38,7 +38,7 @@ homeContactBtn.addEventListener('click', () => {
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    // console.log(1 - window.scrollY / homeHeight);
+    console.log(1 - window.scrollY / homeHeight);
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
@@ -56,6 +56,35 @@ document.addEventListener('scroll', () => {
 arrowUp.addEventListener('click', () => {
     scrollIntoView('#home')
 })
+
+//Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null) {
+        return;
+    }
+
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            console.log(project.dataset.type)    ;
+            if(filter === '*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');            
+            } else {
+                project.classList.add('invisible');  
+            }
+        });
+        projectContainer.classList.remove('anim-out');            
+    }, 300);    
+});
+
+
+
+
+
 
 
 
